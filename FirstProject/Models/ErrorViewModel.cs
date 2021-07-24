d5acf3fc8f018bc08869a550e2ae63c3e39f48d7
kihyun12 <81129734+kihyun12@users.noreply.github.com>
2021-07-24 13:02:55 +0900

    First commit

---
+++ FirstProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace FirstProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
